7d263e0e6d01f752ffb020205e2715cf3d868e7e
hieuchar <hieuvo963@gmail.com>
2018-06-26 15:48:55 -0600

    Added getting config filepath through console

---
+++ RightslineDemoAppDotNet/RightslineDemoAppDotNetV2/ConfigSetup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RightslineDemoAppDotNetV2
+{
+    public static class ConfigSetup
+    {
+        public static string FilePath { get; set; }
+        public static void GetConfigFilePath()
+        {
+            Console.WriteLine("Please input the filepath to the config file: ");
+            FilePath = Console.ReadLine();
+        }
+    }
+}
