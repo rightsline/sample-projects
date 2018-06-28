e1365cb16dc8b2477733fb198ec6242c23eb6f01
hieuchar <hieuvo963@gmail.com>
2018-06-28 13:17:08 -0600

    Added json examples for posting a Feature.

--- RightslineDemoAppDotNet/RightslineDemoAppDotNetV2/RestClient.cs
+++ RightslineDemoAppDotNet/RightslineDemoAppDotNetV2/RestClient.cs
@@ -12,16 +12,14 @@ namespace RightslineDemoAppDotNetV2
     public class RestClient
     {
         public static string BaseConnectionString = "http://api-qa.rightsline.com/v2/";
-        public static string DemoCatalogItem = "catalog-item/1552";
         static HttpClient client = new HttpClient();
-
-
         #region Catalog Item Example Methods
         /// <summary>
         /// Async GET call to Rightsline's V2 API that returns a json string of the catalog-item with a specified ID
         /// Uses Basic Auth Credentials from ConfigSetup class
         /// </summary>
         /// <returns></returns>
+        public static string DemoCatalogItem = "catalog-item/1565";
         public static async Task<string> GetCatalogItemDemoMethod()
         {            
             client.DefaultRequestHeaders.Clear();
@@ -36,14 +34,14 @@ namespace RightslineDemoAppDotNetV2
         /// Asynce POST call to Rightsline's V2 API that creates a new Catalog Item
         /// </summary>
         /// <returns>ID of created Item</returns>
+        public static string CatalogItemEpisodePostExample = "Catalog Item Example JSON/CatalogItemEpisodePOST.json";
+        public static string CatalogItemFeaturePostExample = "Catalog Item Example JSON/CatalogItemFeaturePOST.json";
         public static async Task<string> PostCatalogItemDemoMethod()
         {
             client.DefaultRequestHeaders.Clear();
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Add("Authorization", ConfigSetup.BasicAuthCredentials);            
-            var jsonObj = File.ReadAllText("CatalogItemPostExample.json");
-            //jsonObj["title"] = "hvo-test-dotnet4";
-            Console.WriteLine(jsonObj);
+            var jsonObj = File.ReadAllText(CatalogItemFeaturePostExample);
             var postTask = client.PostAsync(BaseConnectionString + "catalog-item", new StringContent(jsonObj.ToString(), Encoding.UTF8, "application/json"));
             var result = await postTask;
             return result.Content.ReadAsStringAsync().Result;
