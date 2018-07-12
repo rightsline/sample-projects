501589b9086769ef3c90c51594ff935cfa5c826e
Maciej Smusz <maciejsmusz@gmail.com>
2018-07-12 11:26:29 -0600

    Added HMAC signing process.

--- RightslineDemoAppDotNet/RightslineDemoAppDotNetSQS/Program.cs
+++ RightslineDemoAppDotNet/RightslineDemoAppDotNetSQS/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Dynamic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using RightslineDemoAppDotNetSQS.Config;
 
 namespace RightslineDemoAppDotNetSQS
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
+        private static string BaseUrl = "https://sqs.us-west-2.amazonaws.com";
+        RestClient restClient = new RestClient();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = RestClient.Get().Result;
+            Console.WriteLine(result);
         }
     }
 }
--- RightslineDemoAppDotNet/RightslineDemoAppDotNetSQS/RestClient.cs
+++ RightslineDemoAppDotNet/RightslineDemoAppDotNetSQS/RestClient.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using RightslineDemoAppDotNetSQS.Config;
 
 
 namespace RightslineDemoAppDotNetSQS
@@ -10,5 +14,20 @@ namespace RightslineDemoAppDotNetSQS
     public class RestClient
     {
         private static string BaseUrl = "https://sqs.us-west-2.amazonaws.com";
+
+        private static string MyUrl =
+            "https://sqs.us-west-2.amazonaws.com/013474081760/v2_qa_div29.fifo/?Action=ReceiveMessage&MaxNumberOfMessages=10";
+
+        static ConfigSetup _configSetup = new ConfigSetup();
+        static HttpClient client = new HttpClient();
+
+        public static async Task<String> Get()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, MyUrl);
+            var newRequestMessage = await _configSetup.Sign(requestMessage, "sqs");
+            var results = await client.SendAsync(newRequestMessage);
+            
+            return results.Content.ToString();
+        }
     }
 }
