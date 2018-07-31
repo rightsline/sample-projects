5d3319851253adc2e4b3cedd481000cc77347b47
hieuchar <hieuvo963@gmail.com>
2018-07-31 15:20:44 -0600

    Set up java version to get credentials from config file. Added more documentation on C# version

---
+++ RighstlineDemoApplication_Java/RighstlineDemoAppJavaV2/src/com/rightsline/ConfigSetup.java
@@ -0,0 +1,16 @@
+package com.rightsline.Config;
+//import org.json.*;
+
+public class ConfigSetup {
+    public String BasicAuth;
+
+    public String getBasicAuth() {
+        return BasicAuth;
+    }
+
+    public void getConfigFile() {
+
+//        JSONObject obj = new JSONObject(" .... ");
+//        String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+    }
+}
