6f2fbabf910f0de3fff5b77435263f445059450d
hieuchar <hieuvo963@gmail.com>
2018-08-30 10:13:37 -0600

    Minor changes for testing

--- RightslineDemoAppDotNet/RightslineDemoAppDotNetV3/Program.cs
+++ RightslineDemoAppDotNet/RightslineDemoAppDotNetV3/Program.cs
@@ -16,7 +16,11 @@ namespace RightslineDemoAppDotNetV3
             //Console.WriteLine(RestClient.GetTableMethod("1890"));
             //Console.WriteLine(RestClient.GetCatalogItemMethod("1541"));
 
-            //Console.WriteLine("New table created with id: " + RestClient.PostCatalogItemMethod("Catalog Item Example JSON/CatalogItemEpisodePOST.json"));
+            //var jsonObject = File.ReadAllText("Catalog Item Example JSON/CatalogItemEpisodePOST.json");
+            //var encodedJsonBytes = Encoding.UTF8.GetBytes(jsonObject);
+            //var hashedJsonString = RestClient.ComputeSHA256Hash(encodedJsonBytes);
+            //Console.WriteLine(hashedJsonString);
+            Console.WriteLine("New Episode created with id: " + RestClient.PostCatalogItemMethod("Catalog Item Example JSON/CatalogItemEpisodePOST.json"));
             //Console.WriteLine(RestClient.DeleteCatalogItemMethod("1547"));
             //Console.WriteLine(RestClient.UpdateCatalogItemMethod("1542", "Catalog Item Example JSON/CatalogItemEpisodePUT.json"));
 
